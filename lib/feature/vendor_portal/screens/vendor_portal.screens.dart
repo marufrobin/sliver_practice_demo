334bf0379d68ca66bf595a15d559c957365688a9
Maruf Robin <marufrobin5@gmail.com>
2024-09-04 10:38:04 +0600

    text animation

--- lib/feature/vendor_portal/screens/vendor_portal.screens.dart
+++ lib/feature/vendor_portal/screens/vendor_portal.screens.dart
@@ -1,3 +1,4 @@
+import 'package:expandable_text/expandable_text.dart';
 import 'package:flutter/material.dart';
 import 'package:sliver_practice_demo/app_config/fake_data/fake.data.dart';
 
@@ -12,7 +13,15 @@ class VendorPortalScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Vendor Portal'),
       ),
-      body: SelectableText(fakeData.getVendorFakeData().toJson().toString()),
+      body: ExpandableText(
+        fakeData.getVendorFakeData().toJson().toString(),
+        expandText: 'Show JSON',
+        collapseText: 'Hide JSON',
+        linkColor: Colors.blue,
+        expandOnTextTap: true,
+        collapseOnTextTap: true,
+        animation: true,
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app_root.screen.dart';
 
-void main() {
-  runApp(const AppRootScreen());
-}
+void main() => runApp(const ProviderScope(child: AppRootScreen()));
